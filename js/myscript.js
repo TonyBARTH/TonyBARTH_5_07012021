4255f3a7a0a7d2a11c366265c68b6067f9c0f7b6
TonyBARTH <contact@tonybarth.com>
2021-01-27 14:11:23 +0100

    Starting-API-local-connexion

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,29 @@
-var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var response = JSON.parse(this.responseText);
-        console.log(response [0]);
-    }
-};
-request.open("GET", "http://localhost:3000/api/cameras");
-request.send();
+var productList = "";
+
+fetch("http://localhost:3000/api/cameras")
+    /* connexion à l'adresse de l'API */
+    .then (
+        function (response){
+            response.json()
+            /* Si une promesse est bien renvoyée en JSON... */
+            .then (
+                function (cameras) {
+                    cameras.forEach(element => {
+                        productList = productList + '<div class="products-list__item">' + '<img class="products-list__image" src="' + element.imageUrl + '" alt="">' + '<h3 class="products-list__item--name">' + element.name + '</h3>' + '</div>';
+                    });
+                    console.log(productList);
+                    /* On parse les data retournées et on affiche dans la console pour contrôle */
+                    document.querySelector(".products-list").innerHTML = productList
+                    /* On applique le contenu de productList dans le HTML de la page */
+                }
+            )
+        }
+        
+    )
+    /* En cas d'erreur le message suivant apparait */
+    .catch (function (err){
+        alert("Une erreur est survenue : impossible d'afficher les produits.");
+    });
+;
+
+
